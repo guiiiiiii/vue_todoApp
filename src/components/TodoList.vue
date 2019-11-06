83c5a62cc6e686e81191d46481bf7d8b6fb0fd45
guriiiiiii <nocandidates@naver.com>
2019-11-06 10:10:44 +0900

    vue helper 적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
       <transition-group name="list" tag="ul">
-        <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class='shadow'>
-          <span class='checkBtn' v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click='toggleComplete(todoItem, index)'>
+        <li v-for="(todoItem, index) in this.todoItems" v-bind:key="todoItem.item" class='shadow'>
+          <span class='checkBtn' v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click='toggleComplete({ todoItem, index })'>
             <i class="fas fa-check"></i>
           </span>
           <span v-bind:class="{textCompleted:todoItem.completed}">{{ todoItem.item }}</span>
-          <span class='removeBtn' v-on:click='removeTodo(todoItem,index)'>
+          <span class='removeBtn' v-on:click='removeTodo({ todoItem,index })'>
             <i class="fas fa-trash-alt"></i>
           </span>
         </li>
@@ -15,18 +15,32 @@
 </template>
 
 <script>
+import { mapGetters ,mapMutations } from 'vuex';
+
 export default {
   methods:{
-      removeTodo(todoItem,index){
-        this.$store.commit('removeOneItem', {todoItem, index});
-        // this.$emit('removeItem',todoItem, index);
-       
-      },
-      toggleComplete(todoItem, index){
-        // this.$emit('toggleItem',todoItem, index);
-        this.$store.commit('toggleItem',{todoItem, index});
+      ...mapMutations({
+          removeTodo : 'removeOneItem',
+          toggleComplete: 'toggleItem'
+        })
+      // removeTodo(todoItem,index){
+      //   // this.$emit('removeItem',todoItem, index);
+      //   this.$store.commit('removeOneItem', {todoItem, index});
+      // },
+      // toggleComplete(todoItem, index){
+      //   // this.$emit('toggleItem',todoItem, index);
+      //   this.$store.commit('toggleItem',{todoItem, index});
         
-      }
+      // }
+    },
+    computed:{
+      // todoItems(){
+      //   return this.$store.getters.storedTodoItems;
+      // }
+      // ...mapGetters(['storedTodoItems'])
+      ...mapGetters({
+        todoItems:'storedTodoItems'
+      })
     }
   
 }
